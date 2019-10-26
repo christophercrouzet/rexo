4a3c3969846fd95d61e256662e86125359e64175
Christopher Crouzet <christopher.crouzet@gmail.com>
2019-10-26 21:11:00 +1300

    Update the copyright's year range

--- include/rexo.h
+++ include/rexo.h
@@ -1,7 +1,7 @@
 /*
    The MIT License (MIT)
 
-   Copyright (c) 2018 Christopher Crouzet
+   Copyright (c) 2018-2019 Christopher Crouzet
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
